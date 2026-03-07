68e54a566b05f9175c3b252753d791ea48e2801e
ryanhenrqq <ryanh8116@gmail.com>
2026-03-07 18:52:32 -0300

    chore: added path resolver for images directions - to fix broken img links after build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RHSCode/"
+  base: "/RHSCode/",
+  resolve: {
+    alias : {
+      '@ico': path.resolve(__dirname, '.src/res/ico'),
+      '@logo': path.resolve(__dirname, '.src/res/logo'),
+      '@img': path.resolve(__dirname, '.src/res/img')
+    }
+  }
 })
