2682689e371b2a7e5b548da31e461f29c1aa9e1e
ryanhenrqq <ryanh8116@gmail.com>
2026-09-25 14:35:29 -0300

    opt-pt2: code optimization - splitted header ts from main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import { useState, useEffect, lazy, Suspense } from 'react'
 import './App.css'
-import { createHashRouter, RouterProvider, Outlet, Link, useNavigate, useLocation } from 'react-router-dom'
+import { createHashRouter, RouterProvider, Outlet, Link, useLocation } from 'react-router-dom'
 
 const HomePage = lazy(() => import('./components/home/main'))
 const MainPortfolio = lazy(() => import('./components/projects/portfolio'))
 const Direct = lazy(() => import('./components/contact/direct'))
 const RoyaltiesPage = lazy(() => import('./components/royalties'))
 const NotFoundPage = lazy(() => import('./components/404'))
-import type { Language, RedirecterLinkProps, FullHeaderDesktopProps, FullHeaderMobileProps } from './types/types'
+import type { Language } from './types/types'
+import { FullHeaderDesktop, FullHeaderMobile  } from './components/header/header'
 import { useIsMobile } from './types/mobile'
-
-import greenLogo from '/logo-image.png'
-import srvPortfolio from '@ico/services-portfolio.png'
-import srvMessage from '@ico/comment.png'
-import menu from '@ico/menu.png'
 
 const loadingScreen = document.getElementById("loading-screen")
 
 if (loadingScreen) {
   loadingScreen.remove()
 }
-// Small comment for git
 
 const route = createHashRouter([
   {
@@ -72,9 +67,6 @@ const route = createHashRouter([
 ])
 
 function App() {
-  useEffect(() => {
-    console.log("started app.tsx")
-  }, [])
   return <RouterProvider router={route} />
 }
 
@@ -86,7 +78,6 @@ function DesktopHeader() {
     window.scrollTo(0, 0);
   }, [pathname]);
   return (
-    <>
       <main>
         {!mobView?<FullHeaderDesktop classNaming="desktopView full-head-desktop" />:<FullHeaderMobile classNaming="mobileView full-head-mobile" />}
         <div className="main">
@@ -94,132 +85,6 @@ function DesktopHeader() {
         </div>
         <FooterGlobal />
       </main>
-    </>
-  )
-}
-
-function FullHeaderDesktop({classNaming}: FullHeaderDesktopProps) {
-  // below these 2 elements, add <MenuOptionsPopup /> later!!
-  return (
-    <>
-        <header className={classNaming}>
-          <LeftSideHeader />
-          <RightSideHeader />
-        </header>
-    </>
-  )
-}
-
-function FullHeaderMobile({classNaming}: FullHeaderMobileProps) {
-  const location = useLocation()
-  const path = location.pathname.slice(1)
-  const title = path.charAt(0).toUpperCase() + path.slice(1)
-  const navi = useNavigate()
-
-  const [topListen, setTopListen] = useState<boolean>(true)
-  useEffect(() => {
-    const handleHeaderHide = () => {
-      setTopListen(window.scrollY < 100);
-    } 
-    window.addEventListener('scroll', handleHeaderHide, { passive: true })
-    return () => {
-      window.removeEventListener('scroll', handleHeaderHide);
-    }
-  }, [])
-  return (
-    <header className={classNaming}>
-        <div className="left-head flex-hor">
-            <div className="right-head-buttons-parent">
-              <RedirectHeadLink toTarget='portfolio' isActive={location.pathname === "/portfolio"} />
-            </div>
-        </div>
-        <div onClick={() => navi('/')}>
-            {!topListen ? 
-              <h1 className="title-head title-head-index">     {/* Se NAO estiver no topo, ele mostra rhs code se estiver nohome, ou title se nao */}
-                {location.pathname==="/" ? "RHS Code" : title}
-              </h1> : 
-              <h1 className="title-head title-head-index"> {/* Se estiver e nao tiver na home, ele mostra rhs code */}
-                {location.pathname==="/" ? "" : "Home"}
-              </h1>
-            }
-        </div>
-        <nav className="right-head flex-hor">
-            <div id="scrool-head" className="flex-hor right-head-buttons-parent">
-              <RedirectHeadLink toTarget='direct' isActive={location.pathname === "/direct"} />
-            </div>
-        </nav>
-    </header>
-  )
-}
-
-function LeftSideHeader() {
-  const navi = useNavigate()
-  return (
-    <>
-      <div className="left-head flex-hor" onClick={() => navi('/')}>
-          <PhotoLogoChanger />
-      </div>
-    </>
-  )
-}
-
-function PhotoLogoChanger() {
-  const location = useLocation()
-  const path = location.pathname.slice(1)
-  const title = path.charAt(0).toUpperCase() + path.slice(1)
-
-  const [topListen, setTopListen] = useState<boolean>(true)
-  useEffect(() => {
-    const handleHeaderHide = () => {
-      setTopListen(window.scrollY < 100);
-    }
-    window.addEventListener('scroll', handleHeaderHide, { passive: true })
-    return () => {
-      window.removeEventListener('scroll', handleHeaderHide);
-    }
-  }, [])
-  return (
-    <>
-      {!topListen ? <img src={greenLogo} alt="Logo RHS Sites" className="image-head" id="image-head" loading="lazy" /> : null}
-      {!topListen ? <h1 className="title-head title-head-index">{location.pathname==="/" ? "RHS Code" : title}</h1> : <h1 className="title-head title-head-index">{location.pathname==="/" ? "" : "Voltar"}</h1>}
-    </>
-  )
-}
-
-function RightSideHeader() {
-  //const location = useLocation()
-  const [activeMenu, setActiveMenu] = useState(false)
-  const marker = activeMenu ? {filter: "invert(100%)"} : {filter: "invert(0%)"}
-  function handleMenuBool() {
-    activeMenu ? setActiveMenu(false) : setActiveMenu(true)
-  }
-  return (
-    <>
-      <nav className="right-head flex-hor">
-            <div id="scrool-head" className="flex-hor right-head-buttons-parent">
-              <RedirectHeadLink toTarget='portfolio' isActive={location.pathname === "/portfolio"} />
-              <RedirectHeadLink toTarget='direct' isActive={location.pathname === "/direct"} />
-              <button className="tempHide" style={marker} onClick={handleMenuBool}>
-                <img src={menu} alt={menu} className="button-head-image" loading="lazy" />
-              </button>
-            </div>
-        </nav>
-    </>
-  )
-}
-
-
-
-export function RedirectHeadLink({toTarget, isActive}: RedirecterLinkProps) {
-  const local = `/${toTarget}`
-  const marker = isActive ? {filter: "invert(100%)"} : {filter: "invert(0%)"}
-  const imgsrc = toTarget=="portfolio" ? srvPortfolio : srvMessage
-  return (
-    <>
-      <Link to={isActive ? "/" : local} className="button-head c-head gaussian-blur-darker" style={marker}>
-        <img src={imgsrc} alt={local} className="button-head-image" loading="lazy" />
-      </Link>
-    </>
   )
 }
 
---
+++ src/components/header/header.tsx
@@ -0,0 +1,133 @@
+import './header.css'
+
+import type { FullHeaderDesktopProps, FullHeaderMobileProps, RedirecterLinkProps } from '../../types/types'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+import greenLogo from '/logo-image.png'
+import menu from '@ico/menu.png'
+import srvPortfolio from '@ico/services-portfolio.png'
+import srvMessage from '@ico/comment.png'
+
+// HEADER DESKTOP
+export function FullHeaderDesktop({classNaming}: FullHeaderDesktopProps) {
+    return(
+        <header className={classNaming}>
+          <LeftSideHeader />
+          <RightSideHeader />
+        </header>
+    )
+}
+
+function LeftSideHeader() {
+  const navi = useNavigate()
+  return (
+    <>
+      <div className="left-head flex-hor" onClick={() => navi('/')}>
+          <PhotoLogoChanger />
+      </div>
+    </>
+  )
+}
+
+function RightSideHeader() {
+  //const location = useLocation()
+  const [activeMenu, setActiveMenu] = useState(false)
+  const marker = activeMenu ? {filter: "invert(100%)"} : {filter: "invert(0%)"}
+  function handleMenuBool() {
+    activeMenu ? setActiveMenu(false) : setActiveMenu(true)
+  }
+  return (
+    <>
+      <nav className="right-head flex-hor">
+            <div id="scrool-head" className="flex-hor right-head-buttons-parent">
+              <RedirectHeadLink toTarget='portfolio' isActive={location.pathname === "/portfolio"} />
+              <RedirectHeadLink toTarget='direct' isActive={location.pathname === "/direct"} />
+              <button className="tempHide" style={marker} onClick={handleMenuBool}>
+                <img src={menu} alt={menu} className="button-head-image" loading="lazy" />
+              </button>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+function PhotoLogoChanger() {
+  const location = useLocation()
+  const path = location.pathname.slice(1)
+  const title = path.charAt(0).toUpperCase() + path.slice(1)
+
+  const [topListen, setTopListen] = useState<boolean>(true)
+  useEffect(() => {
+    const handleHeaderHide = () => {
+      setTopListen(window.scrollY < 100);
+    }
+    window.addEventListener('scroll', handleHeaderHide, { passive: true })
+    return () => {
+      window.removeEventListener('scroll', handleHeaderHide);
+    }
+  }, [])
+  return (
+    <>
+      {!topListen ? <img src={greenLogo} alt="Logo RHS Sites" className="image-head" id="image-head" loading="lazy" /> : null}
+      {!topListen ? <h1 className="title-head title-head-index">{location.pathname==="/" ? "RHS Code" : title}</h1> : <h1 className="title-head title-head-index">{location.pathname==="/" ? "" : "Voltar"}</h1>}
+    </>
+  )
+}
+
+// HEADER MOBILE
+export function FullHeaderMobile({classNaming}: FullHeaderMobileProps) {
+  const location = useLocation()
+  const path = location.pathname.slice(1)
+  const title = path.charAt(0).toUpperCase() + path.slice(1)
+  const navi = useNavigate()
+
+  const [topListen, setTopListen] = useState<boolean>(true)
+  useEffect(() => {
+    const handleHeaderHide = () => {
+      setTopListen(window.scrollY < 100);
+    } 
+    window.addEventListener('scroll', handleHeaderHide, { passive: true })
+    return () => {
+      window.removeEventListener('scroll', handleHeaderHide);
+    }
+  }, [])
+  return (
+    <header className={classNaming}>
+        <div className="left-head flex-hor">
+            <div className="right-head-buttons-parent">
+              <RedirectHeadLink toTarget='portfolio' isActive={location.pathname === "/portfolio"} />
+            </div>
+        </div>
+        <div onClick={() => navi('/')}>
+            {!topListen ? 
+              <h1 className="title-head title-head-index">     {/* Se NAO estiver no topo, ele mostra rhs code se estiver nohome, ou title se nao */}
+                {location.pathname==="/" ? "RHS Code" : title}
+              </h1> : 
+              <h1 className="title-head title-head-index"> {/* Se estiver e nao tiver na home, ele mostra rhs code */}
+                {location.pathname==="/" ? "" : "Home"}
+              </h1>
+            }
+        </div>
+        <nav className="right-head flex-hor">
+            <div id="scrool-head" className="flex-hor right-head-buttons-parent">
+              <RedirectHeadLink toTarget='direct' isActive={location.pathname === "/direct"} />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+// FUNÇOES E COMPONENTES
+export function RedirectHeadLink({toTarget, isActive}: RedirecterLinkProps) {
+  const local = `/${toTarget}`
+  const marker = isActive ? {filter: "invert(100%)"} : {filter: "invert(0%)"}
+  const imgsrc = toTarget=="portfolio" ? srvPortfolio : srvMessage
+  return (
+    <>
+      <Link to={isActive ? "/" : local} className="button-head c-head gaussian-blur-darker" style={marker}>
+        <img src={imgsrc} alt={local} className="button-head-image" loading="lazy" />
+      </Link>
+    </>
+  )
+}
